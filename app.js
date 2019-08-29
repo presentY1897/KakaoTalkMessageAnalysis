470fd7c06c2d53d1473d969c8ababd65d72c5c78
presentYMac <presenty1897@gmail.com>
2019-08-29 19:28:45 +0900

    set up minimal server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-include 
+var express = require("express");
+var app = express();
+var cors =require('cors');
+var router = require('./router/main')(app);
+
+// view 경로 설정
+app.set('views', __dirname + '/views');
+
+app.use(cors());
+
+// 화면 engine을 ejs로 설정
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
+
+var server = app.listen(4000, function(){
+    console.log("Express server has started on port 4000")
+});
