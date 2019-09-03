90215f2fa4403a9a9b7c57528939a416138f669d
presentYMac <presenty1897@gmail.com>
2019-09-03 19:46:07 +0900

    read text file

--- public/js/fileReader.js
+++ public/js/fileReader.js
@@ -6,19 +6,41 @@ dropHandler = function (e) {
     e.preventDefault();
 
     var files = e.dataTransfer.files,
-        reader = new FileReader();
-    reader.onload = function (event) {
-        console.log(event.target);
-    }
-    console.log('check');
-    console.log(files)
+        reader = new FileReader(),
+        l = files.item.length;
 
+    reader.onload = function(){
+        file_check = reader.result;
+        var lines = file_check.split('\n');
+        var n = lines.size();
+
+        lines.forEach(line => {
+            var cell = line.split(' ');
+        });
+    }
+
+    for (i = 0; i < l; i++) {
+        var file = files[i];
+        if (fileCheck(file)) {
+            reader.readAsText(file, "euc-kr");
+        }
+    }
     return true;
 }
 
-dragOverHandler = function(ev){
-    console.log('File(s) in drop zone'); 
-  
+dragOverHandler = function (ev) {
+    console.log('File(s) in drop zone');
+
     // Prevent default behavior (Prevent file from being opened)
     ev.preventDefault();
-  }
+}
+
+fileCheck = function () {
+    return true;
+}
+
+processFile = function(file){
+    var reader = new FileReader();
+
+    reader.onload
+}
