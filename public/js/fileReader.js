941c1346625f45c67b91fa1df0517ccfa147aa62
presentYMac <presenty1897@gmail.com>
2019-09-05 20:03:37 +0900

    update create data mock up

--- public/js/fileReader.js
+++ public/js/fileReader.js
@@ -1,5 +1,6 @@
 // drag and drop test 
 // https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
+var debug_var;
 dropHandler = function (e) {
     console.log("check drop start");
 
@@ -9,14 +10,28 @@ dropHandler = function (e) {
         reader = new FileReader(),
         l = files.item.length;
 
-    reader.onload = function(){
+    reader.onload = function () {
         file_check = reader.result;
         var lines = file_check.split('\n');
-        var n = lines.size();
+        var n = lines.length,
+            talkingData = [];
 
         lines.forEach(line => {
             var cell = line.split(' ');
+            if (cell.length > 5) // TODO clear
+                talkingData.push({ // TODO too long message check, and exit message check
+                    name: cell[5],
+                    time: cell[0] + cell[1] + cell[2] + cell[3],
+                    message: cell[7]
+                })
+            //talkingData.push(new talkData(cell[5], cell[0] + cell[1] + cell[2] + cell[3], cell[7]))
         });
+
+        debug_var = talkingData;
+        var countTalks = d3.nest()
+            .key(function (d) { return d.name; })
+            .rollup(function (v) { return d3.sum(v, function (d) { return 1; }); })
+            .entries(debug_var);
     }
 
     for (i = 0; i < l; i++) {
@@ -39,7 +54,7 @@ fileCheck = function () {
     return true;
 }
 
-processFile = function(file){
+processFile = function (file) {
     var reader = new FileReader();
 
     reader.onload
---
+++ public/js/talkdata.js
@@ -0,0 +1,7 @@
+// 이름, 시간, 메세지 내용
+// TODO talkData builder
+talkData = function(name, time, message) {
+    this.name = name;
+    this.time = time;
+    this.message = message;
+}
