09a957a885d0918fa607ae97155b60853458972d
presentYMac <presenty1897@gmail.com>
2019-09-22 10:48:31 +0900

    update read data func

---
+++ public/js/dataSculp.js
@@ -0,0 +1,14 @@
+sculpture = function(){
+    var columns, styles;
+
+    column = function(idx){
+        return columns[idx];
+    }
+
+    sculpture = function(){
+
+        return this;
+    }
+
+    return this;
+}
--- public/js/fileReader.js
+++ public/js/fileReader.js
@@ -18,7 +18,7 @@ dropHandler = function (e) {
 
         lines.forEach(line => {
             var cell = line.split(' ');
-            if (cell.length > 5) // TODO check clearly not message line
+            if (cell.length > 5 && dateCheck(makeDate(cell))) // TODO check clearly not message line
                 talkingData.push({ // TODO too long message check, and exit message check
                     name: cell[5],
                     time: cell[0] + cell[1] + cell[2] + cell[3],
@@ -54,28 +54,54 @@ dragOverHandler = function (ev) {
     ev.preventDefault();
 }
 
-fileCheck = function () {
+makeDate = function (cell) {
+    if (cell.length > 4) {
+        var year = cell[0],
+            month = cell[1],
+            day = cell[2],
+            miltime = cell[3],
+            hour = cell[4].split(":")[0],
+            minute = cell[4].split(":")[1].split(","),
+            _date = new Date(year, month, day, hour, minute);
+        if (miltime == '오전') {
+            return _date;
+        } else {
+            return _date.setHours(hour + 12);
+        }
+
+    }
+    return false;
+}
+
+dateCheck = function (_date) {
+    // https://stackoverflow.com/questions/1353684/detecting-an-invalid-date-date-instance-in-javascript
+    if (!(_date instanceof Date && !isNaN(_date)))
+        return false;
     return true;
 }
 
-closeDragDropBox = function(div){
+fileCheck = function () {
+    return true; // TODO check function
+}
+
+closeDragDropBox = function (div) {
     //  div.target.hidden = true;
     div.target.style.display = "none";
 }
 
-createCanvas = function(data){
+createCanvas = function (data) {
     var svg = d3.select("body").append("svg")
-    .attr("width", "100%")
-    .attr("height", "100%"),
-    bar_width = 20; // TODO relative
+        .attr("width", "100%")
+        .attr("height", "100%"),
+        bar_width = 20; // TODO relative
     bar_height = 10;
 
     svg.selectAll("bar")
-    .data(data)
-    .enter().append("rect")
-    .style("fill", "steelblue")
-    .attr("x", function(d){ return ((data.findIndex(function(e){return e == d}) + 1) * bar_width + 2)})
-    .attr("width",bar_width)
-    .attr("y", "0px")
-    .attr("height", function(d){ console.log(d.value); return d.value; });
+        .data(data)
+        .enter().append("rect")
+        .style("fill", "steelblue")
+        .attr("x", function (d) { return ((data.findIndex(function (e) { return e == d }) + 1) * bar_width + 2) })
+        .attr("width", bar_width)
+        .attr("y", "0px")
+        .attr("height", function (d) { console.log(d.value); return d.value; });
 }
