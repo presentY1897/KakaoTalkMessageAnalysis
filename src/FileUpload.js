ab4b4943e084358f10629ac60a94d79cd48bf7c0
presenty1897 <presenty1897@gmail.com>
2020-10-07 20:14:00 +0900

    init file upload button

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 function FileUpload() {
+  const [fileValue, setValue] = useState('');
+  const inputChange = useCallback(e => {
+      setValue(e.target);
+  })
   return (
     <div className="FileUpload">
-      <input className="FileInputBox">
-      </input>
-      <button className="FileUploadButton">업로드</button>
+      <input placeholder='파일을 올려주세요' className="FileInputBox" value={fileValue} onChange={inputChange}/>
+      <button type="submit" className="FileUploadButton">업로드</button>
     </div>
   );
 }
