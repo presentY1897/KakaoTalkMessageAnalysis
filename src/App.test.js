17d946983466db15eb8f60155ebd1d4afd2dee31
presenty1897 <presenty1897@gmail.com>
2020-10-07 20:13:41 +0900

    make file input not null case

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent } from '@testing-library/react';
 import FileUpload from './FileUpload';
 
 describe('<FileUpload />', () => {
   it('has upload button', () => {
     const { getByText } = render(<FileUpload />);
     getByText(/업로드/);
-  })
+  });
+
+  it('upload file null check', () =>{
+    const { getByText, getByPlaceholderText } = render(<FileUpload />);
+    const input = getByPlaceholderText(/파일을 올려주세요/);
+    const button = getByText(/업로드/);
+
+    fireEvent.change(input, {
+      target: 'testcase'
+    });
+
+    fireEvent.click(button);
+    expect(input.value).not.toBeUndefined();
+    expect(input.value).not.toBeNull();
+  });
 });
