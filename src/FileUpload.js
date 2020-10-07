48e041a076a1d680f9680f5baf45492c7fb723e9
presenty1897 <presenty1897@gmail.com>
2020-10-07 19:51:38 +0900

    make file upload component

---
+++ src/FileUpload.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FileUpload() {
+  return (
+    <div className="FileUpload">
+      <input className="FileInputBox">
+      </input>
+      <button className="FileUploadButton">업로드</button>
+    </div>
+  );
+}
+
+export default FileUpload;
