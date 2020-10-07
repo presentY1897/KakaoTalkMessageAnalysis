944ba92e5dbb29462600031c68721e58fb744255
presenty1897 <presenty1897@gmail.com>
2020-10-07 19:51:14 +0900

    add upload button have test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import FileUpload from './FileUpload';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<FileUpload />', () => {
+  it('has upload button', () => {
+    const { getByText } = render(<FileUpload />);
+    getByText(/업로드/);
+  })
 });
