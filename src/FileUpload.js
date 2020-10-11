d45de9cb840dd918917fc0ce988d4578d6a57659
presenty1897 <presenty1897@gmail.com>
2020-10-11 11:09:04 +0900

    fix input file not using value

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,13 +1,14 @@
 import React, { useState, useCallback } from 'react';
 
 function FileUpload() {
-  const [fileValue, setValue] = useState('');
+  const [fileValue, setValue] = useState([]);
   const inputChange = useCallback(e => {
-      setValue(e.target);
-  })
+      setValue([e.target.files[0]]);
+      console.log(e.target.files[0]);
+  }, [])
   return (
     <div className="FileUpload">
-      <input type='file' placeholder='파일을 올려주세요' className="FileInputBox" value={fileValue} onChange={inputChange}/>
+      <input type='file' placeholder='파일을 올려주세요' className="FileInputBox" onChange={inputChange}/>
       <button type="submit" className="FileUploadButton">확인</button>
     </div>
   );
