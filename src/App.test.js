5d0451ad97b6cdced46047361fd493fa91421801
presenty1897 <presenty1897@gmail.com>
2020-10-14 00:14:53 +0900

    refactoring

--- src/App.test.js
+++ src/App.test.js
@@ -3,24 +3,23 @@ import { render, fireEvent } from '@testing-library/react';
 import FileUpload from './FileUpload';
 
 describe('<FileUpload />', () => {
+  const { container, getByText, getByPlaceholderText } = render(<FileUpload />);
+  const input = getByPlaceholderText(/파일을 올려주세요/);
+  const button = getByText(/확인/);
   it('has upload button', () => {
-    const { getByText } = render(<FileUpload />);
-    getByText(/확인/);
+    expect(input).toBeTruthy();
   });
 
+  const testTextFile = new File(['테스트'], '테스트.txt', {type: 'text/plain'});
   it('upload file null check', () =>{
-    const { container, getByText, getByPlaceholderText } = render(<FileUpload />);
-    const input = getByPlaceholderText(/파일을 올려주세요/);
-    const button = getByText(/확인/);
-
     fireEvent.change(input, {
       target: {
-        files :[new File(['테스트'], '테스트.txt', {type: 'text/plain'})]
+        files :[testTextFile]
       }
     });
 
     fireEvent.click(button);
-    expect(container.fileValue).not.toBeUndefined();
+    //expect(container.fileValue).not.toBeUndefined();
     expect(container.fileValue).not.toBeNull();
   });
 });
