acbb7ac5b6ec72de7f8e9092c00617a80b0dda4d
presenty1897 <presenty1897@gmail.com>
2020-10-28 20:03:41 +0900

    add csv parse

--- src/TalkingData/Analyzer.js
+++ src/TalkingData/Analyzer.js
@@ -13,10 +13,33 @@ class ChatDataChunk {
 
 class Analyzer {
     resultData = []; 
+    personList = [];
+
+    findPerson(name){
+        const personFinded = this.personList.find(person => person.name === name)
+        if(personFinded !== undefined){
+            return personFinded;
+        } else {
+            const newPerson = new Person();
+            newPerson.name = name;
+            this.personList.push(newPerson);
+            return newPerson;
+        }
+    }
 
     analyzingRawText(raw){
-        const _result = papaparse.parse(raw);
-        console.log(_result);
+        const csvParse = papaparse.parse(raw);
+        const removeHead = csvParse.data.slice(1, csvParse.data.length);
+        this.personList = [];
+        const chunks = removeHead.map(line => {
+            const chunk = new ChatDataChunk();
+            chunk.time = line[0];
+            const person = this.findPerson(line[1]);
+            chunk.person = person;
+            chunk.text = line[2];
+            return chunk;
+        });
+        return chunks;
     }
 }
 
--- src/TalkingData/TalkFile.js
+++ src/TalkingData/TalkFile.js
@@ -3,20 +3,21 @@ import Analyzer from './Analyzer';
 import FileNameBlock from './FileNameBlock';
 
 class TalkFile extends Component {
+  talkData = null;
 
-  getFileName = function(){
+  getFileName = function () {
     let result = '';
     const analyzer = new Analyzer();
 
-    if(this.props.rawFile !== null){
+    if (this.props.rawFile !== null) {
       result = this.props.rawFile.name;
-      const csvResult= this.props.rawFile.text().then(result => analyzer.analyzingRawText(result));
+      this.talkData = this.props.rawFile.text().then(result => analyzer.analyzingRawText(result));
     } else {
       result = '';
     }
     return result;
   }
-  render(){
+  render() {
     return (
       <div>
         <FileNameBlock text={this.getFileName()}></FileNameBlock>
