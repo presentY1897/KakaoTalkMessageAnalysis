d1fa909148a197603c50986e18376b5efa93e53d
presenty1897 <presenty1897@gmail.com>
2020-11-18 18:02:09 +0900

    init divide text analyzer when txt file

--- src/TalkingData/Analyzer.js
+++ src/TalkingData/Analyzer.js
@@ -2,6 +2,8 @@ import papaparse from 'papaparse';
 
 class Person {
     name = '';
+    inTime = null;
+    outTime = null;
 }
 
 class ChatDataChunk {
@@ -12,12 +14,12 @@ class ChatDataChunk {
 }
 
 class Analyzer {
-    resultData = []; 
+    resultData = [];
     personList = [];
 
-    findPerson(name){
+    findPerson(name) {
         const personFinded = this.personList.find(person => person.name === name)
-        if(personFinded !== undefined){
+        if (personFinded !== undefined) {
             return personFinded;
         } else {
             const newPerson = new Person();
@@ -27,7 +29,7 @@ class Analyzer {
         }
     }
 
-    analyzingRawText(raw){
+    analyzingRawCsv(raw) {
         const csvParse = papaparse.parse(raw);
         const removeHead = csvParse.data.slice(1, csvParse.data.length);
         this.personList = [];
@@ -40,6 +42,56 @@ class Analyzer {
             return chunk;
         });
         return chunks;
+    }
+
+    analyzingRawText(text) {
+        const windowPcTypeAnalyzer = (text) => {
+            const rawDeleteRN = text.replace('/r/n', '/n');
+            const rawDeleteR = rawDeleteRN.replace('/r', '/n');
+            const lines = rawDeleteR.split('/n');
+
+            const divideByType = lines.filter((l, idx) => idx > 2)
+                .map(line => {
+                    if (line.indexOf('---------------') === 0) { // 날짜 변경 나중에 정교하게 변경
+                        return { type: 'date', data: line };
+                    } else if (line.indexOf('[') === 0) { // 대화
+                        return { type: 'chat', data: line };
+                    } else { // 나갔거나 대화의 연속
+                        if (line.indexOf('나갔습니다.') > -1) {
+                            return { type: 'out', data: line };
+                        } else if (line.indexOf('들어왔습니다.') > -1) {
+                            return { type: 'in', data: line };
+                        }
+                        return { type: 'someofchat', data: line };
+                    }
+                });
+            let prevChat;
+            let currentTime;
+            const groupingChat = divideByType.map(chunk => {
+                const type = chunk.type;
+                if (type === 'date') {
+                    const time = new Date(chunk.data.replace('---------------', ''));
+                    currentTime = time;
+                } else if (type === 'chat') {
+                    const name = chunk.data.split(']')[0].replace('[', '');
+                    const chatTime = currentTime + chunk.data.split(']')[1].replace('[', ''); //여기 수정
+                    const line = chunk.data.split(']').filter((i, idx) => idx > 1).join(']');
+                    const data = new ChatDataChunk();
+                    chunk.time = chatTime;
+                    chunk.text = line;
+                    const person = this.findPerson(name);
+                    chunk.person = person;
+                    prevChat = data;
+                    return data;
+                } else if (type === 'someofchat') {
+                    prevChat.text = prevChat.text + '\n' + chunk.data;
+                } else if (type === 'out' || type === 'in') {
+                    // 대충 이름 구하는 부분
+                    const person = this.findPerson(name);
+                    person.inTime =
+                }
+            });
+        }
     }
 }
 
