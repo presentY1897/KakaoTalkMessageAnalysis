1a18bcb02abd42ce62a226d87bac906158ab648d
presenty1897 <presenty1897@gmail.com>
2020-11-29 11:58:51 +0900

    mv files

--- src/TalkingData/TalkFilesContainer.js
+++ src/TalkingData/TalkFilesContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Collapse } from 'react-bootstrap';
-import TalkDataViewer from '../DataChart/TalkDataViewer';
+import TalkDataViewer from './TalkDataViewer';
 import TalkFile from './TalkFile';
 
 class TalkFileContainers extends Component {
