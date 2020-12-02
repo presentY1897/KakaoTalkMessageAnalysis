a6a62d6b9ce299132ac1043dfedf64b148df5c7b
Hyunjae Yang, 양현재 <43636876+presentY1897@users.noreply.github.com>
2020-12-02 20:55:58 +0900

    change talkerlist as pagination

--- src/DataChart/TalkerList.js
+++ src/DataChart/TalkerList.js
@@ -1,12 +1,12 @@
-import { ListGroup } from 'react-bootstrap';
+import { ListGroup, Pagination } from 'react-bootstrap';
 import React, { Component } from 'react';
 
 class TalkerList extends Component {
     render() {
+        const list = this.props.people !== undefined ? this.props.people.map(person => <ListGroup.Item active>{person.name}</ListGroup.Item>) : [];
+        // TODO: Pagination
         return (
-            <ListGroup horizontal>{
-                this.props.people !== undefined ? this.props.people.map(person => <ListGroup.Item active>{person.name}</ListGroup.Item>) : ''
-            }</ListGroup>
+            <Pagination size='lg'>{list}</Pagination>
         );
     };
 };
